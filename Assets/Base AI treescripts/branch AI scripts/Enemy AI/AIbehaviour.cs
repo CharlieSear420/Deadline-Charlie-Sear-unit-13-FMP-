69374f858f2175ca9b10ffbc12bee6bf17753198
CharlieSear420 <72386551+CharlieSear420@users.noreply.github.com>
2022-05-04 15:54:30 +0100

    d

--- Assets/Base AI treescripts/branch AI scripts/Enemy AI/AIbehaviour.cs
+++ Assets/Base AI treescripts/branch AI scripts/Enemy AI/AIbehaviour.cs
@@ -4,15 +4,130 @@ using UnityEngine;
 
 public class AIbehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /*
+    * 
+    * In here I want the AI to check wether or not its timer has more time than the players 
+    * if it does have more time than the players it will then check its hand
+    * if it can play an attack card then it will
+    * then if not it will check to see if it can play a special card
+    * then if not it will see if it can play a defense card
+    * then if not it will have drawn a new card and it will repeat the thought process 
+    * 
+    */
+
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+
+    //-----------------------------decision making-----------------------------------
+
+    public void ThoughtProcess()
+    {
+        if (CheckTimer() == true)
+        {
+            if (CheckOffensiveCard() == true)
+            {
+                if (CheckBurnerCard() == true)
+                {
+                    PlayOffensive();
+                }
+                else
+                {
+                    if (CheckSpecialCard() == true)
+                    {
+                        PlaySpecialCrad();
+                    }
+                }
+            }
+            else
+            {
+                if (CheckSpecialCard() == true)
+                {
+                    if (CheckSpecialCost() == true)
+                    {
+                        PlaySpecialCrad();
+                    }
+                }
+            }
+            
+            
+            if (CheckDefensiveCard() == true)
+            {
+                if (CheckBoardSpace() == true)
+                {
+                    PlayDefensiveCard();
+                }
+            }
+            
+        }
+        else
+        {
+            if (CheckDefensiveCard() == true)
+            {
+                if (CheckBoardSpace() == true)
+                {
+                    PlayDefensiveCard();
+                }
+            }
+        }
+    }
+
+    //-----------------------------checks---------------------------------------------
+
+    public void CheckTimer()
+    {
+
+    }
+    public void CheckOffensiveCard()
+    {
+
+    }
+    public void CheckBurnerCard()
+    {
+
+    }
+    public void CheckSpecialCard()
+    {
+
+    }
+    public void CheckSpecialCost()
+    {
+
+    }
+    public void CheckDefensiveCard()
+    {
+
+    }
+    public void CheckBoardSpace()
+    {
+
+    }
+
+    //---------------------------play cards------------------------------------------
+
+    public void PlayOffensive()
+    {
+
+    }
+    public void PlaySpecialCrad()
+    {
+
+    }
+    public void PlayDefensiveCard()
+    {
+
+    }
+
+
+
 }
